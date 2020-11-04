247680f5be8309edb73628b4457a7eecff0e0ec9
Bruno Morozini dos Santos <bruno.santos@corp.csc.local>
2020-11-03 22:12:21 -0300

    first commit

---
+++ AULA 2.py
@@ -0,0 +1,12 @@
+a = 10
+b = 5
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisão = a / b
+resto = a % b
+print("soma: ", soma)
+print(subtracao)
+print(multiplicacao)
+print(divisão)
+print(resto) 
